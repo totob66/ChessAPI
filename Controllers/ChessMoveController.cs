cb235b2d1800acef9e3d7d49cd7be0a431af7d2d
Håvard Tobiassen <haavard.tobiassen@geodata.no>
2023-07-30 12:13:18 +0200

    Test

--- Controllers/ChessMoveController.cs
+++ Controllers/ChessMoveController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using HaaToChess;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-
+// Haavard var her
 namespace repos.Controllers
 {
     [ApiController]
